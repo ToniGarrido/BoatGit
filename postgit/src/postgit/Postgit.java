c148c4a2c5af2bf00f2d960ee6b6b2dbb6500870
Toni <Toni@DESKTOP-S7MLKAG>
2016-05-10 00:54:36 +0200

    segon

---
+++ postgit/src/postgit/Postgit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package postgit;
+
+/**
+ *
+ * @author Toni
+ */
+public class Postgit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic herededewdwrwyjukytrewryutrefwrymjumytrewrtmuytre
+        
+    }
+    
+}
