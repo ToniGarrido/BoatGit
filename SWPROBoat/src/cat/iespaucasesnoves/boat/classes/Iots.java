83093c10232554b4efa1be530970cb4f7bad9451
Toni <Toni@DESKTOP-S7MLKAG>
2016-05-17 16:47:10 +0200

    Hem afegit totes les classes

--- SWPROBoat/src/cat/iespaucasesnoves/boat/classes/Iots.java
+++ SWPROBoat/src/cat/iespaucasesnoves/boat/classes/Iots.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package swproboat;
+package cat.iespaucasesnoves.boat.classes;
 
 /**
  *
- * @author miquel
+ * @author Toni
  */
-public class Prova {
+public class Iots {
     
 }
