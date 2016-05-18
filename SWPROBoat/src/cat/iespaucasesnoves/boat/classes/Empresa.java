e0709b03ecebe8febfb6480cdc9947f18197a2e1
miquel <miquel@Miquelpc>
2016-05-18 19:05:14 +0200

    Classe Empresa amb tots els metodes creats(falten acabar); llistes fetes

--- SWPROBoat/src/cat/iespaucasesnoves/boat/classes/Empresa.java
+++ SWPROBoat/src/cat/iespaucasesnoves/boat/classes/Empresa.java
@@ -1,6 +1,7 @@
 package cat.iespaucasesnoves.boat.classes;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Empresa {
@@ -12,65 +13,109 @@ public class Empresa {
     private HashMap<Integer, Lloguer> llistaLloguers;
     private HashMap<Integer, Model> llistaModel;
     private HashMap<Integer, Client> llistaClients;
-    
-    public void Empresa(String nom){
-    
-    }
-    
-    public void afegirReparacio(Reparacio reparacio){
-        
-    }
-    public void afegirLloguer(Lloguer lloguer){
-        
-    }
-    public void afegirVenda(Venda venda){
-        
-    }
-    public void afegirVaixell(Vaixell vaixell){
+
+    public void Empresa(String nom) {
 
     }
-    
-    public void afegirClient(Client client){
-        
+
+    public void afegirReparacio(Reparacio reparacio) {
+
     }
-        
-    public void afegirEmpleat(Empleat empleat){
-        
+
+    public void afegirLloguer(Lloguer lloguer) {
+
+    }
+
+    public void afegirVenda(Venda venda) {
+
+    }
+
+    public void afegirVaixell(Vaixell vaixell) {
+
+    }
+
+    public void afegirClient(Client client) {
+
+    }
+
+    public void afegirEmpleat(Empleat empleat) {
+
+    }
+
+    public void afegiPatro(Patro patro) {
+
+    }
+
+    public Model llistarMoDisponibles(Model model) {
+
+        return model;
+    }
+
+    public Model llistarTipusEmb(Model model) {
+
+        return model;
+    }
+
+    public ArrayList<Model> llistarIntervalPreu(Double preuMinim, Double preuMaxim) {
+        ArrayList<Model> modelsInterval = new ArrayList();
+        return modelsInterval;
+    }
+
+    public ArrayList<Reparacio> llistarRepaPendent() {
+
+        ArrayList<Reparacio> repaPendents = new ArrayList();
+        return repaPendents;
+    }
+
+    public ArrayList<Reparacio> llistarRepaAturada() {
+
+        ArrayList<Reparacio> repaAturades = new ArrayList();
+        return repaAturades;
+    }
+
+    public ArrayList<Reparacio> llistarRepaVaixell(Vaixell vaixell) {
+
+        ArrayList<Reparacio> repaVaixell = new ArrayList();
+        return repaVaixell;
+    }
+
+    public ArrayList<Empleat> ferNomina() {
+        ArrayList<Empleat> nomines = new ArrayList();
+        return nomines;
     }
     
-   public void afegiPatro(Patro patro){
-   
-   } 
+    public ArrayList<Vaixell> llistarVaixellDispData(Date dataInicial, Date dataFinal){
+        
+        ArrayList<Vaixell> vaixellsDisp = new ArrayList();
+        return vaixellsDisp;
+    }
     
-   public Model llistarMoDisponibles(Model model){
-   
-       return model;
-   }
-   
-   public Model llistarTipusEmb(Model model){
+    public void eliminarVaixell(Vaixell vaixell){
     
-       return model;
-   }
-   
-   public ArrayList<Model> llistarIntervalPreu(Double preuMinim, Double preuMaxim){
-    ArrayList 
-       return llista;
-   }
-   
-   public void (){
-   
-   }
-   
-   public void (){
-   
-   }
-   
-   public void (){
-   
-   }
-   
-   public void (){
-   
-   }
-   
+    }
+    
+    public void eliminarPatro(Patro patro){
+    
+    }
+    
+    public void eliminarReparacio(Reparacio reparacio){
+    
+    }
+    
+    public void eliminarEmpleat(Empleat empleat){
+    
+    }
+    
+    public void eliminarLloguer(Lloguer lloguer){
+    
+    }
+    
+    public void eliminarModel(Model model){
+    
+    }
+    
+    public void eliminarClient(Client client){
+    
+    }
+    
 }
