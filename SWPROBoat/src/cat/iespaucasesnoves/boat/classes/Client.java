6adf65e8af16d29d91e4bbb29bce625a8d17c301
Joan <Joan@Joan-PC>
2016-05-18 18:33:40 +0200

    He afegit els atributs i constructor a la classe Client

--- SWPROBoat/src/cat/iespaucasesnoves/boat/classes/Client.java
+++ SWPROBoat/src/cat/iespaucasesnoves/boat/classes/Client.java
@@ -1,5 +1,19 @@
 package cat.iespaucasesnoves.boat.classes;
 
-public class Client {
+public class Client extends Persona {
 
+    private Pagament formaPagament;
+
+    public Client(String nom, String llinatge, boolean dni, boolean nie, boolean passaport, String numeroDocument, String adreça, int telefon, String email, Pagament formaPagament) {
+
+        this.nom = nom;
+        this.llinatge = llinatge;
+        this.dni = dni;
+        this.nie = nie;
+        this.numeroDocument = numeroDocument;
+        this.adreça = adreça;
+        this.telefon = telefon;
+        this.email = email;
+        this.formaPagament = formaPagament;
+    }
 }
