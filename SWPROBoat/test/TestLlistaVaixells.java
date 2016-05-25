6ad946a1e5bb219f09702e26f0bea93701da211f
miquel <miquel@Miquelpc>
2016-05-25 18:34:42 +0200

    Tests dels Vaixells acabat.

--- SWPROBoat/test/TestLlistaVaixells.java
+++ SWPROBoat/test/TestLlistaVaixells.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import cat.iespaucasesnoves.boat.classes.Empresa;
 import cat.iespaucasesnoves.boat.classes.Iots;
@@ -17,10 +12,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author miquel
- */
 public class TestLlistaVaixells {
 
     @Test
@@ -38,26 +29,9 @@ public class TestLlistaVaixells {
 
     }
 
-    @Test(expected = LimitCaracterString.class)
-    public void afegirVaixellException() throws LimitCaracterString, IdRepetit {
-        Empresa empresaProva = new Empresa("EmpresaProva");
-        Iots iot01 = new Iots("qwertyuiopasdfghjklñzxcvbnmqwertyuiopasdfghjk", 70.5, 50.8, 40.7, 80000, "Pacific", true, 5, true, 500, 150);
-        Vaixell vaixellProva01 = new Vaixell(iot01, "456789BFR", false);
-        empresaProva.afegirVaixell(vaixellProva01);
-
-    }
-     @Test(expected = IdRepetit.class)
-    public void afegirVaixellException2() throws IdRepetit, LimitCaracterString {
-        Empresa empresaProva = new Empresa("EmpresaProva");
-        Iots iot01 = new Iots("iot1", 70.5, 50.8, 40.7, 80000, "Pacific", true, 5, true, 500, 150);
-        Vaixell vaixellProva01 = new Vaixell(iot01, "456789BFR", false);
-        empresaProva.afegirVaixell(vaixellProva01);
-        empresaProva.afegirVaixell(vaixellProva01);
-
-    }
     @Test
-    public void esborrarVaixell() throws LimitCaracterString, NoHiEsLlista, IdRepetit{
-    Empresa empresaProva = new Empresa("EmpresaProva");
+    public void esborrarVaixell() throws LimitCaracterString, NoHiEsLlista, IdRepetit {
+        Empresa empresaProva = new Empresa("EmpresaProva");
         Iots iot01 = new Iots("Toyota", 70.5, 50.8, 40.7, 80000, "Pacific", true, 5, true, 500, 150);
         Iots iot02 = new Iots("Renault", 80.6, 78.1, 56.2, 90000, "El Mari", false, 6, true, 600, 190);
         Vaixell vaixellProva01 = new Vaixell(iot01, "456789BFR", false);
@@ -70,13 +44,31 @@ public class TestLlistaVaixells {
         empresaProva.eliminarVaixell(vaixellProva02);
         assertEquals("S'ha esborrat un vaixell correctament", 1, empresaProva.getLlistaVaixells().size());
     }
-    
-     @Test(expected = NoHiEsLlista.class)
-    public void esborrarVaixellException() throws LimitCaracterString, NoHiEsLlista {
+
+    @Test(expected = LimitCaracterString.class)
+    public void excepcioCaracters() throws LimitCaracterString, IdRepetit {
         Empresa empresaProva = new Empresa("EmpresaProva");
-       Iots iot01 = new Iots("VaixellFantasma", 70.5, 50.8, 40.7, 80000, "Pacific", true, 5, true, 500, 150);
-       Vaixell vaixellFantasma = new Vaixell(iot01, "456789BFR", false);
+        Iots iot01 = new Iots("qwertyuiopasdfghjklñzxcvbnmqwertyuiopasdfghjk", 70.5, 50.8, 40.7, 80000, "Pacific", true, 5, true, 500, 150);
+        Vaixell vaixellProva01 = new Vaixell(iot01, "456789BFR", false);
+        empresaProva.afegirVaixell(vaixellProva01);
+
+    }
+
+    @Test(expected = IdRepetit.class)
+    public void excepcioIdRepetit() throws IdRepetit, LimitCaracterString {
+        Empresa empresaProva = new Empresa("EmpresaProva");
+        Iots iot01 = new Iots("iot1", 70.5, 50.8, 40.7, 80000, "Pacific", true, 5, true, 500, 150);
+        Vaixell vaixellProva01 = new Vaixell(iot01, "456789BFR", false);
+        empresaProva.afegirVaixell(vaixellProva01);
+        empresaProva.afegirVaixell(vaixellProva01);
+
+    }
+
+    @Test(expected = NoHiEsLlista.class)
+    public void excepcioNoHiEsLlista() throws LimitCaracterString, NoHiEsLlista {
+        Empresa empresaProva = new Empresa("EmpresaProva");
+        Iots iot01 = new Iots("VaixellFantasma", 70.5, 50.8, 40.7, 80000, "Pacific", true, 5, true, 500, 150);
+        Vaixell vaixellFantasma = new Vaixell(iot01, "456789BFR", false);
         empresaProva.eliminarVaixell(vaixellFantasma);
-        
-}
+    }
 }
